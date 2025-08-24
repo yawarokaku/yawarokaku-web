0b5ab5cc2fd27016eef44367ec15e9991c011e06
Katsuya Endoh <studio@enkatsu.org>
2025-08-24 14:57:10 +0900

    update 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,7 +5,10 @@ import Layout from '../components/Layout'
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
     <Layout>
-      <h1 className="text-2xl">404</h1>
+      <div className="max-w-4xl mx-auto">
+        <h1 className="text-2xl">404</h1>
+        <div className="text-3xl">Not Found</div>
+      </div>
     </Layout>
   )
 }
