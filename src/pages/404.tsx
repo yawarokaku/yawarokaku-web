42a2f82a7cf60365b309f3aeb8448f955f11adb8
Katsuya Endoh <studio@enkatsu.org>
2025-08-24 14:59:27 +0900

    update 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,8 +6,8 @@ const NotFoundPage: React.FC<PageProps> = () => {
   return (
     <Layout>
       <div className="max-w-4xl mx-auto">
-        <h1 className="text-2xl">404</h1>
-        <div className="text-3xl">Not Found</div>
+        <h1 className="text-3xl">404</h1>
+        <div className="text-2xl">Not Found</div>
       </div>
     </Layout>
   )
